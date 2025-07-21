0f206a70ebf3fec30a86e460a3252f27ff1f8b53
dmlee <dmlee@forbiz.co.kr>
2025-07-21 10:11:44 +0900

    헤더 검색 기능 추가

--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -1,24 +1,32 @@
 'use client'
 
 import * as header from './Header.css';
-import React, { useState, ChangeEvent } from 'react';
+import React, { useRef, useState, ChangeEvent, useEffect } from 'react';
 
 export default function Search() {
+    const inputRef = useRef<HTMLInputElement | null>(null);
     const [keyword, setKeyword] = useState('');
+    const [isDeleteButton, setIsDeleteButton] = useState(false);
 
-    const writeKeyword = (event: ChangeEvent) => {
+    useEffect(() => {
+        // 검색어 존재여부에 따른 삭제 버튼 노출 처리
+        if (keyword) {
+            setIsDeleteButton(true);
+        } else {
+            setIsDeleteButton(false);
+        }
+    }, [keyword]);
+
+    const writeKeyword = (event: ChangeEvent<HTMLInputElement>) => {
         const value = (event.target as HTMLInputElement).value;
         setKeyword(value);
     };
     
     const deleteKeyword = () => {
-        setKeyword('');
+        setKeyword(''); // 키워드 삭제
+        inputRef.current?.focus(); // input focus
     };
     
-    const searchKeyword = () => {
-        
-    };
-
     return (
         <div className={header.searchBox}>
             <input 
@@ -28,7 +36,10 @@ export default function Search() {
                 className={header.searchBoxInput}
                 value={keyword}
                 onChange={writeKeyword}
+                ref={inputRef}
             />
+            {isDeleteButton 
+            &&
             <button 
                 type="button" 
                 className={header.searchBoxDelButton}
@@ -36,10 +47,10 @@ export default function Search() {
             >
                 검색어 삭제
             </button>
+            }
             <button 
                 type="button" 
                 className={header.searchBoxButton}
-                onClick={searchKeyword}
             >
                 검색
             </button>
